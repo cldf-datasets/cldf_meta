e37c895e530c05c240a80b0fd8568f1c943cfea4
Johannes Englisch <englisch@shh.mpg.de>
2022-01-26 10:33:44 +0100

    download data with sickle

--- cldfbench_clld_meta.py
+++ cldfbench_clld_meta.py
@@ -7,6 +7,9 @@ import time
 from urllib import request
 from xml.etree import ElementTree as ET
 
+from sickle import Sickle
+from sickle.iterator import OAIResponseIterator
+
 from cldfbench import Dataset as BaseDataset
 
 
@@ -43,106 +46,25 @@ def dc_ns(elem):
     return '{http://purl.org/dc/elements/1.1/}%s' % elem
 
 
-def wait_a_minute(msg):
-    sys.stderr.write(msg)
-    sys.stderr.write('\n')
-    secs = 60
-    interval = 5
-    while secs > 0:
-        sys.stderr.write('{}..'.format(secs))
-        # flush the stream -- a status bar that only updates
-        # when everything is done isn't particularly useful
-        sys.stderr.flush()
-        time.sleep(interval)
-        secs -= interval
-    sys.stderr.write('0\n')
-
-
-class MetaDataDownloader:
-
-    def __init__(self):
-        self.rate_limit_remaining = None
-
-    def next_batch(self, url):
-        if self.rate_limit_remaining == 0:
-            wait_a_minute(
-                'We hit the rate limit\n'
-                'Waiting 60s before next request, '
-                "so Zenodo doesn't get mad at us...\n")
-        # try three times
-        retries = 3
-        for n in range(retries):
-            with request.urlopen(url) as response:
-                self.rate_limit_remaining = response.headers.get(
-                    'X-RateLimit-Remaining')
-                if response.status == 200:
-                    # ok
-                    return response.read()
-                elif response.status == 429:
-                    # too many requests
-                    wait_a_minute(
-                        'Request failed due to rate limit\n'
-                        'Waiting 60s before retrying, '
-                        "so Zenodo doesn't get mad at us...\n"
-                        '(attempt {} of {})'.format(n + 1, retries))
-                else:
-                    print(
-                        'Unexpected http response:', response.status,
-                        '\nRetrying (attempt', n + 1, 'of', '%s)...' % retries,
-                        file=sys.stderr)
-        else:
-            print('Tried 3 times to no avail.  Giving up...', file=sys.stderr)
-            return None
-
-
 def zenodo_records():
     # TODO find a way to search for all records
     #  (ideally on the server-side, rather than downloading *all* the records)
 
-    dl = MetaDataDownloader()
+    dl = Sickle(
+        OAI_URL,
+        retry_status_codes=[503, 429],
+        max_retries=3,
+        default_retry_after=60,
+        iterator=OAIResponseIterator)
 
-    # TODO escape community
     #set='user-dictionaria')
-    url = '{url}?verb={verb}&metadataPrefix={md_prefix}&set={set}'.format(
-        url=OAI_URL,
-        verb='ListRecords',
-        md_prefix='oai_dc',
-        set='user-lexibank')
+    for resp in dl.ListRecords(metadataPrefix='oai_dc', set='user-lexibank'):
+        xml_data = ET.fromstring(resp.raw)
 
-    # TODO remove debug code
-    import time
-    nr = 0
-    date = time.strftime('%Y-%m-%d-%Hh%Mm%Ss', time.localtime())
-
-    download_more = True
-    while download_more:
-        download_more = False
-
-        raw_data = dl.next_batch(url)
-        if not raw_data:
-            break
-
-        # TODO remove debug code
-        nr += 1
-        with open('tmp/{}-{}'.format(date, nr), 'wb') as f:
-            f.write(raw_data)
-
-        xml_data = ET.fromstring(raw_data.decode('utf-8'))
-
+        # TODO get info out of sickle's pre-parsed record objects
         records = xml_data.find(oai_ns('ListRecords')).iter(oai_ns('record'))
         for xml_record in records:
             yield xml_record
-
-        resumption_token_xml = xml_data\
-            .find(oai_ns('ListRecords'))\
-            .find(oai_ns('resumptionToken'))
-        resumption_token = getattr(resumption_token_xml, 'text', None)
-        if resumption_token:
-            url = '{url}?verb={verb}&resumptionToken={token}'.format(
-                url=OAI_URL,
-                verb='ListRecords',
-                token=resumption_token)
-            download_more = True
 
 
 def _parse_md(elem):
--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setup(
     },
     install_requires=[
         'cldfbench',
+        'sickle',
     ],
     extras_require={
         'test': [
